58685fcbdcfb6dc250e2523ce2fb020a43cc63b4
LukasGillich <44360438+LukasGillich@users.noreply.github.com>
2021-07-30 22:13:46 +0200

    Update Main.py restructuring the code

--- EmotionIdentityExtractor/Main.py
+++ EmotionIdentityExtractor/Main.py
@@ -3,8 +3,9 @@ import EmotionDetector as ed
 import os
 import time
 import cv2
-#import eval_script as ev
+# import eval_script as ev
 import VAClassifier as va
+
 
 def get_time_stamps(path_to_video):
     starttime = time.time()
@@ -34,31 +35,17 @@ def process_video(path_to_video, path_to_results_dir):
         os.makedirs(result_dir)
 
     img_names, time_stamps = fx.save_face_imgs_to_dir(path_to_video, result_dir, display=True)
-    #time_stamps = get_time_stamps(path_to_video)
+    # time_stamps = get_time_stamps(path_to_video)
     ed.save_emotion_labels_to(result_dir, time_stamps, display=True)
-    #ev.save_valence_arousal_to(result_dir, time_stamps, display=True)
+    # ev.save_valence_arousal_to(result_dir, time_stamps, display=True)
     va.save_valence_arousal_to(result_dir, time_stamps, display=True)
 
 
-#videoname = "39-25-424x240.mp4"
-#videoname = "vid1.mp4"
-#videoname = "vid9.mp4"
-#videoname = "1_4.avi"
-#videoname = "vid5.mp4"
-#videoname = "330.mp4"
-#videoname = "vid3.mp4"
-#videoname = "vid22.mp4"
-#path_to_video_dir = os.path.join("res", "vids", videoname)
-#save_results_in_dir = os.path.join("imgs", "test4")
+##################################################################
+# Edit here the path to your video and the path to an output directory
 
+video_path = os.path.join("C:", "Desktop", "test.mp4")
+save_results_in_dir = os.path.join("imgs")
+process_video(video_path, save_results_in_dir)
 
-videoname = "2021-03-19-16_51_32.avi"
-
-
-user = "user_1"
-site = "site_1"
-#path_to_video_dir = os.path.join("imgs", "participants", user, site, "video", videoname)
-path_to_video_dir = os.path.join("imgs", "git_test", "participants", user, site, "video", videoname)
-#save_results_in_dir = os.path.join("imgs", "participants", user, site, "output")
-save_results_in_dir = os.path.join("imgs", "git_test", "participants", user, site, "output")
-process_video(path_to_video_dir, save_results_in_dir)
+###################################################################
